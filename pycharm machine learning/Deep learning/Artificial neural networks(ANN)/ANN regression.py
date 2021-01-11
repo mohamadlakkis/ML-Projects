b6794bb0ca1c7f0a3071ee70668e0a62e4dacf78
mohamadlakkis <74541089+mohamadlakkis@users.noreply.github.com>
2021-01-11 12:14:03 +0200

    Update ANN regression.py

--- pycharm machine learning/Deep learning/Artificial neural networks(ANN)/ANN regression.py
+++ pycharm machine learning/Deep learning/Artificial neural networks(ANN)/ANN regression.py
@@ -16,7 +16,7 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
 '''	
-####	
+#####
 Building the ANN	
 ####	
 '''
