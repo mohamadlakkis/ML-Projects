0d1e61fe84592ed8fc27c02a09ee1eaa25c2d7ef
mohamadlakkis <74541089+mohamadlakkis@users.noreply.github.com>
2021-01-11 12:14:35 +0200

    Update ANN tut.py

--- pycharm machine learning/Deep learning/Artificial neural networks(ANN)/ANN tut.py
+++ pycharm machine learning/Deep learning/Artificial neural networks(ANN)/ANN tut.py
@@ -30,7 +30,7 @@ x = np.array(ct.fit_transform(x))
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
 
-'''
+'''#
 feature scaling
 '''
 
